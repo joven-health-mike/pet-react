45b71edf33f453ff9e135861affc523da610256f
Mike Burke <mike@jovenhealth.com>
2024-03-16 11:17:43 -0600

    Clean up home page

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -3,33 +3,45 @@
 import React from "react"
 import styled from "styled-components"
 import { buttonStyles } from "../styles/mixins"
-import { Grid } from "@mui/material"
+import { Grid, Typography } from "@mui/material"
 import Navbar from "../navbar/Navbar"
 import { useNavigate } from "react-router-dom"
 
 const CustomButton = styled.button`
   ${buttonStyles}
+  width: 100%;
 `
 
 const HomePage: React.FC = () => {
   let navigate = useNavigate()
-
-  const onRunPayrollClicked = () => {
-    navigate("/payroll")
-  }
-
-  const onRunInvoicesClicked = () => {
-    navigate("/invoices")
-  }
 
   return (
     <>
       <nav>
         <Navbar />
       </nav>
-      <Grid container direction="column" alignItems="center" margin={5}>
-        <CustomButton onClick={onRunPayrollClicked}>Run Payroll</CustomButton>
-        <CustomButton onClick={onRunInvoicesClicked}>Run Invoices</CustomButton>
+      <Typography variant="h4" sx={{ mt: 5 }}>
+        Home
+      </Typography>
+      <Grid container sx={{ p: 10 }}>
+        <Grid item xs={12} sm={12} md={12} lg={6} xl={4} sx={{ p: 1 }}>
+          <CustomButton onClick={() => navigate("/payroll")}>
+            Payroll
+          </CustomButton>
+        </Grid>
+        <Grid item xs={12} sm={12} md={12} lg={6} xl={4} sx={{ p: 1 }}>
+          <CustomButton onClick={() => navigate("/invoices")}>
+            Invoices
+          </CustomButton>
+        </Grid>
+        <Grid item xs={12} sm={12} md={12} lg={6} xl={4} sx={{ p: 1 }}>
+          <CustomButton onClick={() => navigate("/transactions")}>
+            Transactions
+          </CustomButton>
+        </Grid>
+        <Grid item xs={12} sm={12} md={12} lg={6} xl={4} sx={{ p: 1 }}>
+          <CustomButton onClick={() => navigate("/tools")}>Tools</CustomButton>
+        </Grid>
       </Grid>
     </>
   )
