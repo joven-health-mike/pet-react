ea4d75737ee8d3c2c0d4f895eb655515117fc5ea
Mike Burke <mike@jovenhealth.com>
2024-03-21 13:11:12 -0600

    Pull more stuff from analytics page

--- src/components/data-widgets/NoShowDataSection.tsx
+++ src/components/data-widgets/NoShowDataSection.tsx
@@ -1,6 +1,6 @@
 // Copyright 2022 Social Fabric, LLC
 
-import React from "react"
+import React, { useEffect, useState } from "react"
 import DefaultHeader from "../widgets/DefaultHeader"
 import {
   Accordion,
@@ -10,19 +10,85 @@ import {
 } from "@mui/material"
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown"
 import NoShowChart from "./NoShowChart"
+import Session from "../../data/Session"
+import SessionGroups, { createSessionGroups } from "../../data/SessionGroups"
+import { sortMapByValue } from "../../utils/SortUtils"
 
 const CUSTOMER_CHART_LABEL = "No-Show Rates by Customer"
 const PROVIDER_CHART_LABEL = "No-Show Rates by Provider"
 
 type NoShowDataSectionProps = {
-  customerData: Map<string, number>
-  providerData: Map<string, number>
+  sessions: Session[]
 }
 
-const NoShowDataSection: React.FC<NoShowDataSectionProps> = ({
-  customerData,
-  providerData,
-}) => {
+const NoShowDataSection: React.FC<NoShowDataSectionProps> = ({ sessions }) => {
+  const [customerNoShowData, setCustomerNoShowData] =
+    useState<Map<string, number>>()
+  const [providerNoShowData, setProviderNoShowData] =
+    useState<Map<string, number>>()
+  const [customerSessionGroups, setCustomerSessionGroups] =
+    useState<SessionGroups>()
+  const [providerSessionGroups, setProviderSessionGroups] =
+    useState<SessionGroups>()
+
+  useEffect(() => {
+    if (sessions.length > 0) {
+      setCustomerSessionGroups(
+        createSessionGroups(sessions, (session: Session) => session.schoolName)
+      )
+      setProviderSessionGroups(
+        createSessionGroups(
+          sessions,
+          (session: Session) => session.providerName
+        )
+      )
+    } else {
+      setCustomerSessionGroups(undefined)
+      setProviderSessionGroups(undefined)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [sessions.length])
+
+  useEffect(() => {
+    if (customerSessionGroups !== undefined) {
+      const customerAbsentRates = new Map<string, number>()
+      const customerPresences = new Map<string, number>()
+      const customerAbsences = new Map<string, number>()
+      const customerNames = customerSessionGroups.getNames()
+      customerNames.forEach((customerName) => {
+        const sessionGroup =
+          customerSessionGroups.getSessionGroupForName(customerName)!
+        if (sessionGroup.absentRate() > 0) {
+          customerAbsentRates.set(
+            customerName,
+            parseFloat((sessionGroup.absentRate() * 100).toFixed(3))
+          )
+        }
+        customerPresences.set(customerName, sessionGroup.presences())
+        customerAbsences.set(customerName, sessionGroup.absences())
+      })
+      setCustomerNoShowData(sortMapByValue(customerAbsentRates))
+    }
+  }, [customerSessionGroups])
+
+  useEffect(() => {
+    if (providerSessionGroups !== undefined) {
+      const providerAbsentRates = new Map<string, number>()
+      const providerNames = providerSessionGroups.getNames()
+      providerNames.forEach((providerName) => {
+        const sessionGroup =
+          providerSessionGroups.getSessionGroupForName(providerName)!
+        if (sessionGroup.absentRate() > 0) {
+          providerAbsentRates.set(
+            providerName,
+            parseFloat((sessionGroup.absentRate() * 100).toFixed(3))
+          )
+        }
+      })
+      setProviderNoShowData(sortMapByValue(providerAbsentRates))
+    }
+  }, [providerSessionGroups])
+
   return (
     <>
       <DefaultHeader>No-Show Rates</DefaultHeader>
@@ -36,7 +102,10 @@ const NoShowDataSection: React.FC<NoShowDataSectionProps> = ({
           {CUSTOMER_CHART_LABEL}
         </AccordionSummary>
         <AccordionDetails>
-          <NoShowChart chartTitle={CUSTOMER_CHART_LABEL} data={customerData} />
+          <NoShowChart
+            chartTitle={CUSTOMER_CHART_LABEL}
+            data={customerNoShowData!}
+          />
         </AccordionDetails>
       </Accordion>
       <Accordion>
@@ -48,7 +117,10 @@ const NoShowDataSection: React.FC<NoShowDataSectionProps> = ({
           {PROVIDER_CHART_LABEL}
         </AccordionSummary>
         <AccordionDetails>
-          <NoShowChart chartTitle={PROVIDER_CHART_LABEL} data={providerData} />
+          <NoShowChart
+            chartTitle={PROVIDER_CHART_LABEL}
+            data={providerNoShowData!}
+          />
         </AccordionDetails>
       </Accordion>
     </>
--- src/components/pages/AnalyticsPage.tsx
+++ src/components/pages/AnalyticsPage.tsx
@@ -8,7 +8,6 @@ import Session, { createSession } from "../../data/Session"
 import { adaptTeleTeachersDataForInvoices } from "../../utils/TeleTeachersAdapter"
 import HorizontalLine from "../widgets/HorizontalLine"
 import SessionGroups, { createSessionGroups } from "../../data/SessionGroups"
-import { sortMapByValue } from "../../utils/SortUtils"
 import NoShowDataSection from "../data-widgets/NoShowDataSection"
 import CustomerReportsSection from "../data-widgets/CustomerReportsSection"
 
@@ -16,51 +15,7 @@ const AnalyticsPage: React.FC = () => {
   const [sessions, setSessions] = useState<Session[]>([])
   const [customerSessionGroups, setCustomerSessionGroups] =
     useState<SessionGroups>()
-  const [providerSessionGroups, setProviderSessionGroups] =
-    useState<SessionGroups>()
   const [readyToDisplay, setReadyToDisplay] = useState<boolean>(false)
-  const [customerData, setCustomerData] = useState<Map<string, number>>()
-  const [providerData, setProviderData] = useState<Map<string, number>>()
-
-  useEffect(() => {
-    if (customerSessionGroups !== undefined) {
-      const customerAbsentRates = new Map<string, number>()
-      const customerPresences = new Map<string, number>()
-      const customerAbsences = new Map<string, number>()
-      const customerNames = customerSessionGroups.getNames()
-      customerNames.forEach((customerName) => {
-        const sessionGroup =
-          customerSessionGroups.getSessionGroupForName(customerName)!
-        if (sessionGroup.absentRate() > 0) {
-          customerAbsentRates.set(
-            customerName,
-            parseFloat((sessionGroup.absentRate() * 100).toFixed(3))
-          )
-        }
-        customerPresences.set(customerName, sessionGroup.presences())
-        customerAbsences.set(customerName, sessionGroup.absences())
-      })
-      setCustomerData(sortMapByValue(customerAbsentRates))
-    }
-  }, [customerSessionGroups])
-
-  useEffect(() => {
-    if (providerSessionGroups !== undefined) {
-      const providerAbsentRates = new Map<string, number>()
-      const providerNames = providerSessionGroups.getNames()
-      providerNames.forEach((providerName) => {
-        const sessionGroup =
-          providerSessionGroups.getSessionGroupForName(providerName)!
-        if (sessionGroup.absentRate() > 0) {
-          providerAbsentRates.set(
-            providerName,
-            parseFloat((sessionGroup.absentRate() * 100).toFixed(3))
-          )
-        }
-      })
-      setProviderData(sortMapByValue(providerAbsentRates))
-    }
-  }, [providerSessionGroups])
 
   useEffect(() => {
     if (sessions.length > 0) {
@@ -68,18 +23,9 @@ const AnalyticsPage: React.FC = () => {
       setCustomerSessionGroups(
         createSessionGroups(sessions, (session: Session) => session.schoolName)
       )
-      setProviderSessionGroups(
-        createSessionGroups(
-          sessions,
-          (session: Session) => session.providerName
-        )
-      )
     } else {
       setReadyToDisplay(false)
-      setCustomerData(new Map())
-      setProviderData(new Map())
       setCustomerSessionGroups(undefined)
-      setProviderSessionGroups(undefined)
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [sessions.length])
@@ -103,10 +49,7 @@ const AnalyticsPage: React.FC = () => {
       </>
       {readyToDisplay && (
         <>
-          <NoShowDataSection
-            customerData={customerData!}
-            providerData={providerData!}
-          />
+          <NoShowDataSection sessions={sessions} />
           <HorizontalLine />
           <CustomerReportsSection
             sessions={sessions}
