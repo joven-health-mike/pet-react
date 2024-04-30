e9425b7b5b9163719636b919891e79614229d66c
Mike Burke <mike@jovenhealth.com>
2024-04-30 14:25:33 -0600

    Remove 2nd button from Provider Upload Widget

--- src/components/data-widgets/ProviderReportUploadWidget.tsx
+++ src/components/data-widgets/ProviderReportUploadWidget.tsx
@@ -3,14 +3,13 @@ import UploadDataWidget from "../widgets/UploadDataWidget"
 import { handleUploadDataNew } from "../../utils/DataProcessor"
 import { SessionsContext } from "../../data/providers/SessionProvider"
 import { createSession } from "../../data/Session"
-import { adaptTeleTeachersData } from "../../utils/TeleTeachersAdapter"
 
 type ProviderReportUploadWidgetProps = {
   sessionDataAdapter?: (input: string[][]) => string[][]
 }
 
 const ProviderReportUploadWidget: React.FC<ProviderReportUploadWidgetProps> = ({
-  sessionDataAdapter = adaptTeleTeachersData,
+  sessionDataAdapter,
 }) => {
   const { sessions, setSessions } = useContext(SessionsContext)
 
@@ -18,22 +17,13 @@ const ProviderReportUploadWidget: React.FC<ProviderReportUploadWidgetProps> = ({
     <>
       <UploadDataWidget
         prompt="Provider Report"
-        subPrompt="The Provider Report can be exported from either the **Session Analysis Dashboard (SAD)** or from **TeleTeachers**. Select the option that coorelates with where the data was exported from."
-        button1Text={"Upload SAD Format"}
-        button2Text={"Upload TeleTeachers Format"}
-        enableSecondOption={true}
+        subPrompt="The Provider Report can be exported from **TeleTeachers**. Upload the file here."
+        enableSecondOption={false}
         hasData={sessions.length > 0}
         onDataLoaded={(data: string[][]) => {
           handleUploadDataNew(data, setSessions, createSession)
         }}
         onDataCleared={() => {
-          setSessions([])
-        }}
-        onData2Loaded={(data: string[][]) => {
-          data = sessionDataAdapter ? sessionDataAdapter(data) : data
-          handleUploadDataNew(data, setSessions, createSession)
-        }}
-        onData2Cleared={() => {
           setSessions([])
         }}
       />
